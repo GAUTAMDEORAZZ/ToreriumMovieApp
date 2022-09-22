8100db5d9c4cc6fda9eecf10e633af9551ca60e0
Gautamdeoprasad <gautamdeorazz@gmail.com>
2022-09-22 22:29:04 +0530

    add movie files

---
+++ index.js
@@ -0,0 +1,301 @@
+
+let data=[
+    {
+        "Title": "Teen Titans GO! To the Movies",
+        "Year": "2018",
+        "imdbID": "tt7424200",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BOTZhMTIwZDUtYjZjZS00MmViLTg3NzEtNWE5NzI1NDUwNDJmXkEyXkFqcGdeQXVyODQxMTI4MjM@._V1_SX300.jpg"
+    },
+    {
+        "Title": "The Life and Movies of Ersan Kuneri",
+        "Year": "2022–",
+        "imdbID": "tt13238304",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNDI2N2VlYzQtYzc1My00YjRhLWE3OWMtMzFiN2YyN2ZkN2UzXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_SX300.jpg"
+    },
+    {
+        "Title": "Home Movies",
+        "Year": "1999–2004",
+        "imdbID": "tt0197159",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNDIwMmNjMTItYmM0YS00M2VlLTg5ZTQtYjJjOGY2MjU2NjI3XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "The Movies That Made Us",
+        "Year": "2019–",
+        "imdbID": "tt10681222",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNGY1NzAyYTQtYmQxZi00NjQ3LWFkMzUtYjA0ZjRhYjI0MTRkXkEyXkFqcGdeQXVyNjAzOTE0OTI@._V1_SX300.jpg"
+    },
+    {
+        "Title": "The New Scooby-Doo Movies",
+        "Year": "1972–1973",
+        "imdbID": "tt0068112",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNWEwYTZhMGQtMDU0ZC00NmYyLTk0OWEtMjg1OTY5OTY5N2EyXkEyXkFqcGdeQXVyMTEzNjA2NzM@._V1_SX300.jpg"
+    },
+    {
+        "Title": "A Personal Journey with Martin Scorsese Through American Movies",
+        "Year": "1995",
+        "imdbID": "tt0112120",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BZGRkMWFkZjgtYzc2My00MjljLTkwNTEtMGZhOTUyM2I3OGMxL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "At the Movies",
+        "Year": "1986–2010",
+        "imdbID": "tt0090523",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTFkZmQ5OWEtNjFmMC00ZTI2LWE0ZWUtN2VhOWE1MzdiOGJhXkEyXkFqcGdeQXVyMTQ4NDY5OTc@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Midnight Movies: From the Margin to the Mainstream",
+        "Year": "2005",
+        "imdbID": "tt0457414",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTIzNTc4MzQyMF5BMl5BanBnXkFtZTcwODM1OTc3MQ@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "The Movies",
+        "Year": "2019",
+        "imdbID": "tt10468676",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNzU4NWEwNDItMzMzYy00ZDYyLWIxZjMtMDlkYWVjNjQwYzBjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Top Gear: At the Movies",
+        "Year": "2011",
+        "imdbID": "tt2202611",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTQwMzk2NWEtMjU3ZC00NmNhLWE4NGQtMjQ0OGUyODEwN2U0XkEyXkFqcGdeQXVyMTAwMzM3NDI3._V1_SX300.jpg"
+    },
+    
+        {
+            "Title": "Spider-Man",
+            "Year": "2002",
+            "imdbID": "tt0145487",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BZDEyN2NhMjgtMjdhNi00MmNlLWE5YTgtZGE4MzNjMTRlMGEwXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Spider-Man: No Way Home",
+            "Year": "2021",
+            "imdbID": "tt10872600",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Amazing Spider-Man",
+            "Year": "2012",
+            "imdbID": "tt0948470",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMyOTM4MDMxNV5BMl5BanBnXkFtZTcwNjIyNzExOA@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Spider-Man 2",
+            "Year": "2004",
+            "imdbID": "tt0316654",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMzY2ODk4NmUtOTVmNi00ZTdkLTlmOWYtMmE2OWVhNTU2OTVkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Spider-Man: Homecoming",
+            "Year": "2017",
+            "imdbID": "tt2250912",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNTk4ODQ1MzgzNl5BMl5BanBnXkFtZTgwMTMyMzM4MTI@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Spider-Man 3",
+            "Year": "2007",
+            "imdbID": "tt0413300",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Spider-Man: Into the Spider-Verse",
+            "Year": "2018",
+            "imdbID": "tt4633694",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_SX300.jpg"
+        },
+        {
+            "Title": "The Amazing Spider-Man 2",
+            "Year": "2014",
+            "imdbID": "tt1872181",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NDYxNTg0OV5BMl5BanBnXkFtZTgwODE5NzU1MTE@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Spider-Man: Far from Home",
+            "Year": "2019",
+            "imdbID": "tt6320628",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Spider-Man: The Animated Series",
+            "Year": "1994–1998",
+            "imdbID": "tt0112175",
+            "Type": "series",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMmQ1NzBlYmItNmZkZi00OTZkLTg5YTEtNTI5YjczZjk3Yjc1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+        },
+
+        
+            {
+                "Title": "Thor",
+                "Year": "2011",
+                "imdbID": "tt0800369",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg"
+            },
+            {
+                "Title": "Thor: Ragnarok",
+                "Year": "2017",
+                "imdbID": "tt3501632",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_SX300.jpg"
+            },
+            {
+                "Title": "Thor: The Dark World",
+                "Year": "2013",
+                "imdbID": "tt1981115",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BMTQyNzAwOTUxOF5BMl5BanBnXkFtZTcwMTE0OTc5OQ@@._V1_SX300.jpg"
+            },
+            {
+                "Title": "Thor: Love and Thunder",
+                "Year": "2022",
+                "imdbID": "tt10648342",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BYmMxZWRiMTgtZjM0Ny00NDQxLWIxYWQtZDdlNDNkOTEzYTdlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg"
+            },
+            {
+                "Title": "Team Thor",
+                "Year": "2016",
+                "imdbID": "tt6016776",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BZmEyODRkYmQtMjM3Yi00ZTkxLWI0NWEtOTJhMzBjNWY5MjJjXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"
+            },
+            {
+                "Title": "Thor: Tales of Asgard",
+                "Year": "2011",
+                "imdbID": "tt1667903",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BMTcxOTU4NzIxMV5BMl5BanBnXkFtZTcwNzE3NjAxNQ@@._V1_SX300.jpg"
+            },
+            {
+                "Title": "Team Thor: Part 2",
+                "Year": "2017",
+                "imdbID": "tt6599818",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BODczODMwOTgtODhkOC00YjFiLWIzYmUtZTI3NThhZDE1NDhkXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"
+            },
+            {
+                "Title": "Valhalla - The Legend of Thor",
+                "Year": "2019",
+                "imdbID": "tt8956872",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BOTA0OGZjODctYjg2ZS00YWQ1LWEyZjgtMDZkNmYyZDAwYzg0XkEyXkFqcGdeQXVyMzcwNjA5Mzg@._V1_SX300.jpg"
+            },
+            {
+                "Title": "Almighty Thor",
+                "Year": "2011",
+                "imdbID": "tt1792794",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BMTcwNjI5MTAzNF5BMl5BanBnXkFtZTcwNTcyOTIwNQ@@._V1_SX300.jpg"
+            },
+            {
+                "Title": "Thor: Finding Korg",
+                "Year": "2018",
+                "imdbID": "tt8513006",
+                "Type": "movie",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BNjI0ZmZlYzktZWNhMC00YjYwLThhZDItOGE4MTg1ZDM1YTMxXkEyXkFqcGdeQXVyNDM2MzU3Njc@._V1_SX300.jpg"
+            }
+        
+    
+];
+
+ 
+ async function myFun(){
+
+    
+    event.preventDefault();
+   
+    let movieName=document.getElementById("movie").value
+    console.log(movieName)
+
+    
+    let responce=movieShow(movieName)
+        let data=await responce;
+        console.log(data)
+        let carsole= document.getElementById("carcol");
+    carsole.innerHTML=null;
+        appendMovies(data)
+
+}
+
+  async function movieShow(q){
+    
+    try{
+        let url=`https://www.omdbapi.com/?s=${q}&apikey=e853a317`
+        let res= await fetch(url);
+        let data=await res.json();
+        // console.log(data.Search)
+        return data.Search
+    }
+    catch(err){
+        console.log(`error`,err);
+       
+        // let container= document.getElementById("container");
+        // container.innerHTML=null
+        // let div=document.createElement("div");
+        // let image=document.createElement("img");
+        // image.src="https://th.bing.com/th/id/OIP.j3zb5-B3KILbmCa6jxFJvQHaE8?w=220&h=180&c=7&r=0&o=5&pid=1.7"
+        // div.append(image);
+        // container.append(div)
+
+       
+    }
+
+ }
+
+ function appendMovies(movies){
+    //  console.log(movies)
+    
+
+     let container= document.getElementById("container");
+     container.innerHTML=null
+    
+     movies.forEach(function(elem){
+    
+        let div=document.createElement("div");
+    
+        let image=document.createElement("img");
+        image.src=elem.Poster;
+
+        let title=document.createElement("h3");
+        title.innerText=elem.Title
+
+        let type=document.createElement("p")
+        type.innerText=elem.Type
+
+        let year=document.createElement("p")
+        year.innerText=elem.Year;
+        year.style.color="gray"
+
+        let imdb=document.createElement("p")
+        imdb.innerText="IMDB:"+ elem.imdbID
+        imdb.style.color="RED"
+        
+        // console.log(image,title,type,year,imdb)
+        div.append(image,title,type,year,imdb)
+
+        container.append(div)
+
+    })
+   
+
+ }
+ appendMovies(data);
